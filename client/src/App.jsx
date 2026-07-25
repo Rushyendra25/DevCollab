ca7f178172435b0365e5b43aa8a3867d7925279e
Rushyendra Varma <rushyendravarma@Rushyendras-MacBook-Air.local>
2026-07-25 20:38:08 +0530

    Setup React Project Structure

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-300">
+      <h1 className="text-5xl font-bold text-indigo-600">
+        Welcome to DevCollab 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
